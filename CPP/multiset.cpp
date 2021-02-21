dd0901fe08a44d610f4b33b29ef47ece1582f512
wangxy <957927607@qq.com>
2021-02-21 19:48:59 +0800

    update

---
+++ CPP/multiset.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+// multiset能够保证他内部的数据是有序的
+
+int main()
+{
+    multiset<int> ms;
+
+    // 下方插入时是 4 3 2 1，输出时变为了1 2 3 4
+    for (int i = 4; i > 0; i--)
+        ms.insert(i);
+
+    for (auto k : ms)
+        cout << k << endl;
+}
