f4c2dfccb6c2ba4f36857c14addd71ca8882f0b9
wangxy <957927607@qq.com>
2021-03-16 14:17:46 +0800

    update

--- CPP/unordered_map.cpp
+++ CPP/unordered_map.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 // unordered_map使用方括号[]构建
@@ -17,14 +18,13 @@ int main()
 {
     // 0.初始化
     unordered_map<string, int> init;
-    init = {{"jack",19},{"aba",22}};
+    init = {{"jack", 19}, {"aba", 22}};
 
-    
     unordered_map<string, int> age;
     // 一些经典用法
     // 1.插入 增
-    age.insert({{"jack", 18}, {"tom", 20},{"Peter",33}});
-    age["steven"]=222;
+    age.insert({{"jack", 18}, {"tom", 20}, {"Peter", 33}});
+    age["steven"] = 222;
     mp_display(age);
     cout << "-----------------------------------" << endl;
     // 2.删除
@@ -32,8 +32,8 @@ int main()
     mp_display(age);
     cout << "-----------------------------------" << endl;
     // 3.修改
-    age.at("Peter")=44;
-    age["tom"]=44;
+    age.at("Peter") = 44;
+    age["tom"] = 44;
     mp_display(age);
     cout << "-----------------------------------" << endl;
     // 4.查找
@@ -44,5 +44,9 @@ int main()
         cout << "[" << index->first << ":" << index->second << "]" << endl;
 
     // count是为了找到元素的个数，可以是1或者0
-    cout<<age.count("steven")<<","<<age.count("jjj")<<endl;
+    cout << age.count("steven") << "," << age.count("jjj") << endl;
+
+    // 5. 直接使用STL的函数，查找的是key的最大值
+    unordered_map<int, int> mp5({{3, 1}, {2, 9}, {1, 10}});
+    cout << "maxelement: " << max_element(mp5.begin(), mp5.end())->first << "," << max_element(mp5.begin(), mp5.end())->second << endl;
 }
