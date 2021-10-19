aa59fc8267c3fce618439710e6c2ad0f6caf1645
wangxy <957927607@qq.com>
2021-10-19 21:53:44 +0800

    update

--- OO/singleton.cpp
+++ OO/singleton.cpp
@@ -2,30 +2,47 @@
 
 using namespace std;
 
-class Singleton
+class Singleton_1
 {
 private:
     int a;
-    Singleton() : a(10) {}
-    static Singleton *instance; // = new Singleton();
+    Singleton_1() : a(10) {}
+    // 这样的写法会有问题，即使没人用你，你也会一直存在，有些浪费，所以更好的写法如_2所示
+    static Singleton_1 *instance; // = new Singleton_1();
 
 public:
-    static Singleton *getInstance()
+    static Singleton_1 *getInstance()
     {
-        instance = new Singleton();
+        instance = new Singleton_1();
         return instance;
     }
     int getData()
     {
         return a;
     }
-    ~Singleton() {}
+    ~Singleton_1() {}
 };
 // 如果没有这句，会未定义的引用
-Singleton *Singleton ::instance = nullptr;
+Singleton_1 *Singleton_1 ::instance = nullptr;
+
+// Meyers Singleton
+class Singleton_2
+{
+private:
+    Singleton_2() {}
+
+public:
+    static Singleton_2 &getInstance()
+    {
+        // 只有进入这个函数，这个instance才会被创建
+        static Singleton_2 instance;
+        return instance;
+    }
+    ~Singleton_2() {}
+};
 
 int main()
 {
-    Singleton *instance = Singleton::getInstance();
+    Singleton_1 *instance = Singleton_1::getInstance();
     cout << instance->getData() << endl;
 }
